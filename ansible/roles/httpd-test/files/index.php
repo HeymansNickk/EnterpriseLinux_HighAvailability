3eb67c213c7359b9083ca7e466b8849cfaf981df
Nick Heymans <nick.heymans@student.hogent.be>
2020-11-27 17:15:39 +0100

    Fixed database permission isue and added cockpit monitoring to monitor server

--- ansible/roles/httpd-test/files/index.php
+++ ansible/roles/httpd-test/files/index.php
@@ -1,9 +1,14 @@
 <html>
    <head>
-      <title>Select a MariaDB Database</title>
+      <title>Searching in the database</title>
+      
    </head>
 
    <body>
+   <h1>This page was served by</h1>
+   <?php
+      echo gethostname() . PHP_EOL;
+   ?>
       <form name="form1" method="get" action="search.php">
           <input type="text" placeholder="type here" name="search" aria-label="Search" required>
           <input type="submit" value="Search" name="submit">
